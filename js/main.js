02a8a7e61a532ce296a5d17a3fc0731ee76c4ed0
phplpsummer <sh052100@naver.com>
2021-09-29 08:39:59 +0900

    메인페이지 visual slide버튼 작업미완_210929

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@
 
 /* visual---------------------------------------------- */
 const $slide = $(".slide");
+const $btnSlide = $(".btn_slide_menu li");
 let timer;
 // let enableClick = true;
 
@@ -12,6 +13,28 @@ let timer;
 timer = setInterval(function(){
     goNext();
 },5000);
+
+//미완성
+$(".btn_slide1").on("click",function(e){
+    e.preventDefault();
+
+    $btnSlide.removeClass("on");
+    $(this).addClass("on");
+
+    $slide.animate({marginLeft:"0%"},1000);
+});
+
+$(".btn_slide2").on("click",function(e){
+    e.preventDefault();
+
+    $btnSlide.removeClass("on");
+    $(this).addClass("on");
+
+    $slide.animate({marginLeft:"-100%"},1000,function(){
+        $slide.css({marginLeft:"0%"});
+        $(".slide li").first().appendTo($slide);
+    })
+});
 
 
 //함수설정
