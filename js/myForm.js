e0f802922c8a605180b21f9be487d6fb2418c979
phplpsummer <sh052100@naver.com>
2021-10-14 13:47:19 +0900

    join페이지 유효성검사 적용_211014

---
+++ js/myForm.js
@@ -0,0 +1,65 @@
+let num = /[0-9]/;
+let eng = /[a-zA-Z]/;
+let spc = /[~!@#$%^&+]/;
+let kor = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힝]/;
+
+
+$("input[type=submit]").on("click",function(e){
+
+    if(!isName(2)) { e.preventDefault();}
+    if(!isTxt("userid",8)) { e.preventDefault();}
+    if(!isPwd(8)) { e.preventDefault();}
+
+});
+
+
+function isTxt(name, len){
+
+    if(len === undefined) len = 5;
+    let txt = $("[name="+name+"]").val();
+
+    if(txt.length >= len) {
+        $("[name="+name+"]").parent().find("p").remove();
+        return true;
+    } else {
+        $("[name="+name+"]").parent().find("p").remove();
+        $("[name="+name+"]").parent().append(
+            "<p>해당 입력 항목에는 "+len+"자리 이상 입력하실 수 있습니다..</p>"
+        );
+        return false;
+    }
+}
+
+function isName(len) {
+
+    if(len === undefined) len = 2;
+    let name = $("[name=username]").val();
+
+    if(name.length>=len && (eng.test(name)||kor.test(name))){
+        $("[name=username]").parent().find("p").remove();
+        return true;
+    } else {
+        $("[name=username").parent().find("p").remove();
+        $("[name=username").parent().append(
+            "<p>이름에는 2자리 이상의 한글과 영문만 입력하실 수 있습니다.</p>"
+        );
+        return false;
+    }
+}
+
+function isPwd(len) {
+
+    if (len === undefined) len = 8;
+    let pwd1 = $("[name=userpwd]").val();
+    let pwd2 = $("[name=userpwd2]").val();
+
+    if(pwd1 == pwd2 && pwd1.length>=len && num.test(pwd1)&&eng.test(pwd1)&&spc.test(pwd1)) {
+        $("[name=userpwd]").parent().find("p").remove();
+        return true;
+    } else {
+        $("[name=userpwd]").parent().find("p").remove();
+        $("[name=userpwd]").parent().append(
+            "<p>비밀번호는 영문,특수문자,숫자를 포함하여 "+len+"글자 이상 입력해주세요.</p>"
+        );
+    }
+}
