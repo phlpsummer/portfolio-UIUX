70556dcb51735f7a6ca60252d5b4567af0b0ca03
phplpsummer <sh052100@naver.com>
2021-10-22 12:11:40 +0900

    community페이지 news, gallery페이지 스타일 변경

--- js/community.js
+++ js/community.js
@@ -27,4 +27,22 @@ var swiper = new Swiper(".mySwiper", {
         delay: 5000,
         disableOnInteraction: true //true: 롤링중 스와이프되면 멈춤, false: 롤링중 스와이프되어도 진행.
     },
-  });
+});
+
+// news 타이틀 말줄임표 기능
+for(let i=0; i<4; i++){
+    $(".swiper-slide").eq(i).children("p").each(function(){
+
+        let length = 40; //표시할 글자수 정하기
+    
+        $(this).each(function(){
+    
+            if( $(this).text().length >= length ){
+    
+                $(this).text( $(this).text().substr(0,length)+'...') 
+                //지정할 글자수 이후 표시할 텍스트
+            }
+        });
+    });
+}
+
