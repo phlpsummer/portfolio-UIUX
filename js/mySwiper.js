c11c793c793a1f703e6f4ed70c42a2e58ab9502a
phplpsummer <sh052100@naver.com>
2021-10-25 04:05:33 +0900

    index페이지 gallery 완성

--- js/mySwiper.js
+++ js/mySwiper.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('#visual', {
+const swiper1 = new Swiper('#visual', {
 
     // navigation: {
     //     nextEl: '.swiper-button-next',
@@ -27,3 +27,38 @@ const swiper = new Swiper('#visual', {
 
 // let isOnNum = $(".swiper-pagination span").hasClass("swiper-pagination-bullet-active").eq();
 // console.log(isOnNum);
+
+const swiper2 = new Swiper('#photo>.inner', {
+
+    loop: true,
+    speed: 500,
+    direction: "horizontal", //방향
+    spaceBetween: 0, //사이간격 (px단위)
+    slidesPerView: 4, //하나의 화면당 보여질 패널의 개수 -> 여러개보여야하므로``
+    centeredSlides: true, //슬라이드 가운데 위치
+    grabCursor: true, //마우스 커서 손바닥모양 변경
+
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false //true: 롤링중 스와이프되면 멈춤, false: 롤링중 스와이프되어도 진행.
+    },
+    effect: "coverflow",
+    coverflowEffect: {
+        rotate: 50,  //슬라이드 회전 각도
+        stretch: -150,  //슬라이드 간 거리, 클수록 겹침.
+        depth: 400,  //깊이효과값
+        modifier: 1,  //효과 배수
+        slideShadows: false  //슬라이더 그림자
+    },
+});
+
+var btnStart = document.querySelector(".btnStart");
+var btnStop = document.querySelector(".btnStop");
+
+
+btnStart.onclick = function(){
+    swiper2.autoplay.start();
+}
+btnStop.onclick = function(){
+    swiper2.autoplay.stop();
+}
