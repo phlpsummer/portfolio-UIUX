63bcc31262b70c74b6d5e23f7113517e2a9a76fa
phplpsummer <sh052100@naver.com>
2021-10-26 12:22:46 +0900

    index페이지 photo완성1

--- js/main.js
+++ js/main.js
@@ -12,6 +12,27 @@ contentVid.addEventListener("mouseleave",(e)=>{
 });
 
 
+/* 메인페이지-photo---------------------------------------------- */
+//롤링 배너
+let mg = 0;
+let timer;
 
+timer = setInterval(rolling,20);
 
+$(".slider").on("mouseenter",function(){
+    clearInterval(timer);
+});
+$(".slider").on("mouseleave",function(){
+    timer = setInterval(rolling,20);
+});
+
+function rolling(){
+    if(mg < -300){
+        mg = 0;
+        $(".slider").children("li").eq(0).appendTo(".slider");
+    }
+
+    mg-=2;
+    $(".slider").css({left:mg});
+}
 
