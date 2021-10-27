6950e5a089c3fb720d044dae336287f497d1eb62
phplpsummer <sh052100@naver.com>
2021-10-27 12:10:38 +0900

    메인페이지 comment 보드 swiper적용완료

--- js/mySwiper.js
+++ js/mySwiper.js
@@ -28,30 +28,6 @@ const swiper1 = new Swiper('#visual', {
 // let isOnNum = $(".swiper-pagination span").hasClass("swiper-pagination-bullet-active").eq();
 // console.log(isOnNum);
 
-// const swiper2 = new Swiper('#photo>.inner', {
-
-//     loop: true,
-//     speed: 500,
-//     direction: "horizontal", //방향
-//     spaceBetween: 0, //사이간격 (px단위)
-//     slidesPerView: 4, //하나의 화면당 보여질 패널의 개수 -> 여러개보여야하므로``
-//     centeredSlides: true, //슬라이드 가운데 위치
-//     grabCursor: true, //마우스 커서 손바닥모양 변경
-
-//     autoplay: {
-//         delay: 4000,
-//         disableOnInteraction: false //true: 롤링중 스와이프되면 멈춤, false: 롤링중 스와이프되어도 진행.
-//     },
-//     effect: "coverflow",
-//     coverflowEffect: {
-//         rotate: 50,  //슬라이드 회전 각도
-//         stretch: -150,  //슬라이드 간 거리, 클수록 겹침.
-//         depth: 400,  //깊이효과값
-//         modifier: 1,  //효과 배수
-//         slideShadows: false  //슬라이더 그림자
-//     },
-// });
-
 const swiperPoster = new Swiper('.program', {
 
     // If we need pagination
@@ -87,7 +63,6 @@ btnStop.onclick = function(){
     btnStart.classList.remove("on");
 }
 
-
 const swiperEvent = new Swiper('.event', {
 
     pagination: {
@@ -102,4 +77,25 @@ const swiperEvent = new Swiper('.event', {
     slidesPerView: 1, //하나의 화면당 보여질 패널의 개수
     grabCursor: true, //마우스 커서 손바닥모양 변경
     effect: "fade",
+});
+
+const swiperBoard = new Swiper('.board', {
+
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+
+    loop: true,
+    direction: "vertical", //방향
+    slidesPerView: 1, //하나의 화면당 보여질 패널의 개수
+
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false //true: 롤링중 스와이프되면 멈춤, false: 롤링중 스와이프되어도 진행.
+    },
 });
