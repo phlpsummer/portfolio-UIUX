95db2750f4839b571b1423296d983e9408f61f2a
phplpsummer <sh052100@naver.com>
2021-10-28 13:24:29 +0900

    메인페이지 content영역 작업

--- js/mySwiper.js
+++ js/mySwiper.js
@@ -18,23 +18,17 @@ const swiper1 = new Swiper('#visual', {
     effect: "fade",
 });
 
-const swiperPoster = new Swiper('.program', {
-    
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: 'true'
-    },
+const swiperContent = new Swiper('#content .mySwiper', {
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: false,
+      },
 
     slidesPerView: "auto",
-    spaceBetween: 40,
-    slidesPerGroup: 2,
-    loop: true,
-    loopFillGroupWithBlank: true,
-
-    autoplay: {
-        delay: 4000,
-        disableOnInteraction: true //true: 롤링중 스와이프되면 멈춤, false: 롤링중 스와이프되어도 진행.
-    },
+    // centeredSlides: false,
+    direction: "horizontal", //방향
+    spaceBetween: 30,
+    grabCursor: true, //마우스 커서 손바닥모양 변경
 });
 
 // var btnStart = document.querySelector(".btnAuto .btnStart");
@@ -51,39 +45,3 @@ const swiperPoster = new Swiper('.program', {
 //     btnStart.classList.remove("on");
 // }
 
-const swiperEvent = new Swiper('.event', {
-
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true
-    },
-
-    loop: true,
-    speed: 500,
-    direction: "horizontal", //방향
-    spaceBetween: 20, //사이간격 (px단위)
-    slidesPerView: 1, //하나의 화면당 보여질 패널의 개수
-    grabCursor: true, //마우스 커서 손바닥모양 변경
-    effect: "fade",
-});
-
-const swiperBoard = new Swiper('.board', {
-
-    pagination: {
-        el: ".swiper-pagination",
-        type: "fraction",
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-
-    loop: true,
-    direction: "vertical", //방향
-    slidesPerView: 1, //하나의 화면당 보여질 패널의 개수
-
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: false //true: 롤링중 스와이프되면 멈춤, false: 롤링중 스와이프되어도 진행.
-    },
-});
