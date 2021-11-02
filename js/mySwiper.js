acc847adc5768de1597e9b135a8770094ac30323
phplpsummer <sh052100@naver.com>
2021-11-02 22:17:21 +0900

    메인페이지 content 디자인변경 및 알림판 추가

--- js/mySwiper.js
+++ js/mySwiper.js
@@ -1,21 +1,41 @@
-const swiper1 = new Swiper('#visual', {
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
+// const swiper1 = new Swiper('#visual', {
+//     pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true,
+//     },
 
-    loop: true,
-    speed: 500,
-    direction: "horizontal", //방향
-    spaceBetween: 0, //사이간격 (px단위)
-    slidesPerView: 1, //하나의 화면당 보여질 패널의 개수
-    grabCursor: false, //마우스 커서 손바닥모양 변경
+//     loop: true,
+//     speed: 500,
+//     direction: "horizontal", //방향
+//     spaceBetween: 0, //사이간격 (px단위)
+//     slidesPerView: 1, //하나의 화면당 보여질 패널의 개수
+//     grabCursor: false, //마우스 커서 손바닥모양 변경
 
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: false //true: 롤링중 스와이프되면 멈춤, false: 롤링중 스와이프되어도 진행.
-    },
-    effect: "fade",
+//     autoplay: {
+//         delay: 5000,
+//         disableOnInteraction: false //true: 롤링중 스와이프되면 멈춤, false: 롤링중 스와이프되어도 진행.
+//     },
+//     effect: "fade",
+// });
+
+const swiperBoard = new Swiper('#content .board .notice', {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  
+  slidesPerView: 1,
+  loop: true,
+  direction: "vertical", //방향
+  spaceBetween: 0,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false //true: 롤링중 스와이프되면 멈춤, false: 롤링중 스와이프되어도 진행.
+  },
 });
 
 const swiperContent = new Swiper('#content .mySwiper', {
@@ -31,18 +51,4 @@ const swiperContent = new Swiper('#content .mySwiper', {
     grabCursor: true, //마우스 커서 손바닥모양 변경
     mousewheel: true,
 });
-
-// var btnStart = document.querySelector(".btnAuto .btnStart");
-// var btnStop = document.querySelector(".btnAuto .btnStop");
-
-// btnStart.onclick = function(){
-//     swiperPoster.autoplay.start();
-//     btnStart.classList.add("on");
-//     btnStop.classList.remove("on");
-// }
-// btnStop.onclick = function(){
-//     swiperPoster.autoplay.stop();
-//     btnStop.classList.add("on");
-//     btnStart.classList.remove("on");
-// }
 
