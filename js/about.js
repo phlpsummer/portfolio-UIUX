dcfb7fba7aa97c35c2e96af9c088df40f6864772
phplpsummer <sh052100@naver.com>
2021-11-03 21:47:44 +0900

    about페이지 타이핑모션 추가

--- js/about.js
+++ js/about.js
@@ -1,16 +1,31 @@
 /* about페이지---------------------------------------------- */
+var pos=[];
+pos.push($(".spotlight").offset().top);
+pos.push($(".values").offset().top);
+console.log(pos);
+
 $(window).on("scroll",function(){
     let scroll = $(this).scrollTop();
 
-    if(scroll > 100){
+    if(scroll >= 100 && scroll < pos[0]){
         $(".circle1").addClass("on");
     }
 
-    $(".circle2").css({
-        left: scroll - 300
-    });
+    if(scroll >= pos[0]-700){
+        let currentScroll = scroll - pos[0] +700;
+        $(".circle2").animate({opacity:0.4},1000);
+        $(".circle2").css({
+            left: currentScroll*1.5
+        });
+    }
 
-    if(scroll > 1070){
+    if(scroll >= pos[0]-500 && scroll < pos[1]){
+        $(".spotlight text").animate({strokeDashoffset:0},1500,function(){
+            $(this).addClass("on");
+        });
+    }
+
+    if(scroll >= pos[1]-400){
         $(".values").addClass("on");
     }
 
